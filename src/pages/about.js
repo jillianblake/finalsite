6336047184f979713dd48fbcdfc3ac15a1085060
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 17:29:52 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -41,7 +41,7 @@ const About = (props) => (
                             </header>
                             <p>Jillian Blake is an attorney and owner of Blake Immigration Law since 2015. She grew up in San Francisco, California and earned her bachelor’s degree in international studies from Johns Hopkins University and master’s degree in international relations from the Johns Hopkins School of Advanced International Studies (SAIS). She then obtained her law degree from the University of Michigan Law School. During law school she discovered her passion for immigration and asylum law while interning at Asylum Access—Ecuador and Capital Area Immigrants’ Rights (CAIR) Coalition. Jillian is a member of the Maryland State Bar and the American Immigration Lawyers Association (AILA). She is also currently an adjunct professor of immigration law at George Mason University Law School in Arlington, Virginia. In her free time Jillian enjoys watching movies, writing, watching Cleveland Browns football and Nationals baseball, and spending time with her dog, Cha Cha.</p>
                             <ul className="actions">
-                                <li><Link to="mailto:jillian@blakeimmigrationcom" className="button">jillian@blakeimmigration.com</Link></li>
+                                <li><Link to="<a href = "jillian@blakeimmigration.com">" className="button">jillian@blakeimmigration.com</Link></li>
                             </ul>
                         </div>
                     </div>
