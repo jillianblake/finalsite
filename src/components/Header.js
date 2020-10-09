f01bc92a5c29ffb108085cc7491e68e2267806dd
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 15:52:05 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-import icon from '../assets/images/website-icon.png'
+
 
 const Header = (props) => (
     <header id="header" className="alt">
-         <img src={icon} alt="" />
+        
     </header>
 )
 
