5ccc5dd3925eee75f36b4ced23b73bbc1e1ea244
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 14:23:34 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,15 +17,7 @@ const About = (props) => (
 
         <BannerAbout />
 
-        <div id="main">
-            <section id="one">
-                <div className="inner">
-                    <header className="major">
-                        <h2>Sed amet aliquam</h2>
-                    </header>
-                    <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
-                </div>
-            </section>
+
             <section id="two" className="spotlights">
                 <section>
                     <Link to="/generic" className="image">
