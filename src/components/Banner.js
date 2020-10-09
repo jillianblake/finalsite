befb290f56c24fa79acd9eee8f1080a48a363eb8
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 15:40:41 -0400

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,10 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Hello, welcome to Blake Immigration Law.</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>WE SPECIALIZE IN REMOVAL DEFENSE, ASYLUM, AND FAMILY-BASED IMMIGRATION.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
