1292b6c55267ca50fa66b191b3aa8cf45fcad726
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 14:34:56 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,20 +2,20 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
-import BannerAboutfrom '../components/BannerAbout'
+import BannerAbout from '../components/BannerAbout'
 
 import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
 import pic10 from '../assets/images/pic10.jpg'
 
-const Landing = (props) => (
+const About = (props) => (
     <Layout>
         <Helmet>
             <title>Landing - Forty by HTML5 UP</title>
             <meta name="description" content="Landing Page" />
         </Helmet>
 
-        <BannerLanding />
+        <BannerAbout />
 
         <div id="main">
             <section id="one">
@@ -113,4 +113,4 @@ const Landing = (props) => (
     </Layout>
 )
 
-export default Landing
+export default About
