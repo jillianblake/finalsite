3e3cf3889163b867a07b6f14e75989105430fb89
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 17:06:01 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
-
 const Header = (props) => (
     <header id="header" className="alt">
-        
+      
+        <nav>
+            <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
+        </nav>
     </header>
 )
 
