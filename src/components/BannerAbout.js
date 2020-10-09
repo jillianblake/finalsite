3b49ee4ed3f19361334588c81967a4cc333ba8d5
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 14:57:37 -0400

    Update BannerAbout.js

--- src/components/BannerAbout.js
+++ src/components/BannerAbout.js
@@ -4,11 +4,10 @@ const BannerAbout = (props) => (
     <section id="banner" className="style2">
         <div className="inner">
             <header className="major">
-                <h1>Landing</h1>
+                <h1>About Us</h1>
             </header>
             <div className="content">
-                <p>Lorem ipsum dolor sit amet nullam consequat<br />
-                sed veroeros. tempus adipiscing nulla.</p>
+                <p>Meet Our Team</p>
             </div>
         </div>
     </section>
