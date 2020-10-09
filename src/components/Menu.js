f9eb1e8cd5cc48844ca38bd5251d206581bd4b5c
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 17:14:06 -0400

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -15,8 +15,8 @@ const Menu = (props) => (
     <li><Link onClick={props.onToggleMenu} to="/contact">Contact Us</Link></li>
             </ul>
             <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Get Started</a></li>
-                <li><a href="#" className="button fit">Log In</a></li>
+                <li><a href="#" className="button special fit">Call Us (703) 594-7857</a></li>
+                <li><a href="https://secure.lawpay.com/pages/blakeimmigration/trust" className="button fit">Pay Online</a></li>
             </ul>
         </div>
         <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
