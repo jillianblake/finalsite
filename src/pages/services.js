ee162ae4b7e76076269f24916499e41d190bccd7
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 16:02:33 -0400

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
-import Layout from '../components/layout'
 import BannerServices from '../components/BannerServices'
 
 import pic08 from '../assets/images/pic13.jpg'
@@ -9,7 +7,7 @@ import pic09 from '../assets/images/pic14.jpg'
 import pic10 from '../assets/images/pic15.jpg'
 
 const Services= (props) => (
-    <Layout>
+   
         <Helmet>
             <title>Services</title>
             <meta name="description" content="Landing Page" />
@@ -19,7 +17,7 @@ const Services= (props) => (
 
         
 
-    </Layout>
+    
 )
 
 export default Services
