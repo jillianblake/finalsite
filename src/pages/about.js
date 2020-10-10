1467996bf6d6fc5a357ef36f16e21041a696e903
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 22:41:31 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -54,7 +54,7 @@ const About = (props) => (
                         <div className="inner">
                             <header className="major">
                                 <h3>Ami Patel, Esq.</h3>
-                                <h4>Attorney/Owner</h4>
+                                <h4>Associate Attorney</h4>
                             </header>
                             <p>Ami Patel is an associate attorney at Blake Immigration Law. She has worked at the firm since 2018, starting as a law graduate clerk and becoming an associate attorney in 2019. Ami grew up in Louisville, Kentucky and earned her bachelor’s degree in international relations and affairs from American University. She then obtained her law degree from George Washington University Law School. During law school she participated in the George Washington University Law School Immigration Clinic. She also interned at the Open Society Institute in Cambodia, the American Immigration Council, and the American Bar Association. As the daughter of immigrants, Ami loves that her work helps others find safety in the United States. Ami is a member of the California State Bar and the American Immigration Lawyers Association (AILA). In her free time Ami likes to read, hike, and seek out new restaurants.</p>
                             <ul className="actions">
