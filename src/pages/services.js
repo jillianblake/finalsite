aee3a60d3a0657e6bba096c85aab85ed945eff3f
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-10 19:07:33 -0400

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -14,12 +14,8 @@ const Services = (props) => (
 <BannerServices />
    
 
-            <section id="one">
-                <div className="inner">
+            
          
-
-                    <h2 id="content">Sample Content</h2>
-                    <p>Praesent ac adipiscing ullamcorper semper ut amet ac risus. Lorem sapien ut odio odio nunc. Ac adipiscing nibh porttitor erat risus justo adipiscing adipiscing amet placerat accumsan. Vis. Faucibus odio magna tempus adipiscing a non. In mi primis arcu ut non accumsan vivamus ac blandit adipiscing adipiscing arcu metus praesent turpis eu ac lacinia nunc ac commodo gravida adipiscing eget accumsan ac nunc adipiscing adipiscing.</p>
                     <div className="grid-wrapper">
                         <div className="col-6">
                             <h3>Sem turpis amet semper</h3>
@@ -43,7 +39,7 @@ const Services = (props) => (
                         </div>
                     </div>
 
-            </section>
+       
    
                    
    
