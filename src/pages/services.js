c8b0bcd2eb3a1dbe8d22b67cc9de4f0966622a0c
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 23:48:36 -0400

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -18,12 +18,12 @@ const Services = (props) => (
                 <div className="inner">
                     <header className="major">
                     </header>
-    <p><b>Removal Defense</b></p>
-    <p>Asylum/Withholding of Removal</p>
+    
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
                     <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutru</p>
                     <span className="image left"><img src={pic13} alt="" /><p><b>Removal Defense</b></p>
-    <p>Asylum/Withholding of Removal</p></span>
+    <p>Asylum/Withholding of Removal</p>
+     <p>Convention Against Torture (CAT)</p></span>
                    
                 </div>
             </section>
