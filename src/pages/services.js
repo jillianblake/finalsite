f02101da83e8e0f71ada13639a801266c86ced16
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-10 18:43:50 -0400

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -29,8 +29,7 @@ const Services = (props) => (
                             <h3>Family-Based Immigration and Naturalization</h3>
                             <p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing blandit tempus accumsan.</p>
                         </div>
-                    </div>
-
+                  
         
     </Layout>
 )
