c9309a637eeb1c09863328ee494af35173cff864
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-10 18:50:23 -0400

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -13,17 +13,14 @@ const Services = (props) => (
     
 <BannerServices />
     
-< /br>
-    < /br>
+
                    
    
-                     <div id="main" className="alt">
+                
 
             <section id="one">
                 <div className="inner">
-                    <header className="major">
-                        <h1>Elements</h1>
-                    </header>
+                  
 
                     <h2 id="content">Sample Content</h2>
                     <p>Praesent ac adipiscing ullamcorper semper ut amet ac risus. Lorem sapien ut odio odio nunc. Ac adipiscing nibh porttitor erat risus justo adipiscing adipiscing amet placerat accumsan. Vis. Faucibus odio magna tempus adipiscing a non. In mi primis arcu ut non accumsan vivamus ac blandit adipiscing adipiscing arcu metus praesent turpis eu ac lacinia nunc ac commodo gravida adipiscing eget accumsan ac nunc adipiscing adipiscing.</p>
