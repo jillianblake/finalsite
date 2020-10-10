56106623d5105c8c33fd6697e7f2d0852514cbbb
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-10 19:11:40 -0400

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -26,15 +26,16 @@ const Services = (props) => (
                             <p>In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim aliquet. Accumsan ac integer lobortis commodo ornare aliquet accumsan erat tempus amet porttitor. Ante commodo blandit adipiscing integer semper orci eget. Faucibus commodo adipiscing mi eu nullam accumsan morbi arcu ornare odio mi adipiscing nascetur lacus ac interdum morbi accumsan vis mi accumsan ac praesent.</p>
                         </div>
                         <div className="col-4">
-                            <h3>Interdum sapien gravida</h3>
+                            <h3>Removal Defense</h3>
+                            <p> Asylum/Withholding of Removal
+ </p>
+                        </div>
+                        <div className="col-4">
+                            <h3>Humanitarian Visas</h3>
                             <p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing blandit tempus accumsan.</p>
                         </div>
                         <div className="col-4">
-                            <h3>Faucibus consequat lorem</h3>
-                            <p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing blandit tempus accumsan.</p>
-                        </div>
-                        <div className="col-4">
-                            <h3>Accumsan montes viverra</h3>
+                            <h3>Family-based Immigration and Naturalizatin </h3>
                             <p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing blandit tempus accumsan.</p>
                         </div>
                     </div>
