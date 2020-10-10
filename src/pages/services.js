902b163ab8eccdf255b741f06e849e506975c2fd
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-10 18:33:59 -0400

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -13,15 +13,22 @@ const Services = (props) => (
     
 <BannerServices />
     
-  <h2 id="content">Sample Content</h2>
-                    <p>Praesent ac adipiscing ullamcorper semper ut amet ac risus. Lorem sapien ut odio odio nunc. Ac adipiscing nibh porttitor erat risus justo adipiscing adipiscing amet placerat accumsan. Vis. Faucibus odio magna tempus adipiscing a non. In mi primis arcu ut non accumsan vivamus ac blandit adipiscing adipiscing arcu metus praesent turpis eu ac lacinia nunc ac commodo gravida adipiscing eget accumsan ac nunc adipiscing adipiscing.</p>
+
                     <div className="grid-wrapper">
                         <div className="col-6">
-                            <h3>Sem turpis amet semper</h3>
+                            <h3>Removal Defense</h3>
+                            <p>Asylum/Withholding of Removal</p>
+    <p>Convention Against Torture (CAT) Protection</p>
+                        </div>
+                        <div className="col-6">
+                            <h3>Humanitarian Visas</h3>
+                            <p>In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim aliquet. Accumsan ac integer lobortis commodo ornare aliquet accumsan erat tempus amet porttitor. Ante commodo blandit adipiscing integer semper orci eget. Faucibus commodo adipiscing mi eu nullam accumsan morbi arcu ornare odio mi adipiscing nascetur lacus ac interdum morbi accumsan vis mi accumsan ac praesent.</p>
+                        </div>
+    <h3>Family-Based Immigration</h3>
                             <p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat commodo eu sed ante lacinia. Sapien a lorem in integer ornare praesent commodo adipiscing arcu in massa commodo lorem accumsan at odio massa ac ac. Semper adipiscing varius montes viverra nibh in adipiscing blandit tempus accumsan.</p>
                         </div>
                         <div className="col-6">
-                            <h3>Magna odio tempus commodo</h3>
+                            <h3>Naturalization</h3>
                             <p>In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim aliquet. Accumsan ac integer lobortis commodo ornare aliquet accumsan erat tempus amet porttitor. Ante commodo blandit adipiscing integer semper orci eget. Faucibus commodo adipiscing mi eu nullam accumsan morbi arcu ornare odio mi adipiscing nascetur lacus ac interdum morbi accumsan vis mi accumsan ac praesent.</p>
                         </div>
                         <div className="col-4">
