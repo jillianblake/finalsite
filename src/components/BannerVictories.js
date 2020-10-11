cba0b3e4e0ecd6b9cb8b09c5bdd0e56768c2c7e0
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-10 22:37:30 -0400

    Create BannerVictories.js

---
+++ src/components/BannerVictories.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BannerVictories = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Victories</h1>
+            </header>
+            <div className="content">
+                <p>See what we've done</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerVictories
