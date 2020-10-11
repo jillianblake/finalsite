81cc90226824dcad96b87f38c27e1fe650fa286e
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-10 22:43:28 -0400

    Create contact.js

---
+++ src/pages/contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+import BannerContact from '../components/BannerContact'
+
+
+const Landing = (props) => (
+    <Layout>
+        <Helmet>
+            <title>Contact</title>
+            <meta name="description" content="Contact Page" />
+        </Helmet>
+
+
+
+    </Layout>
+)
+
+export default Landing
