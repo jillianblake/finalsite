92f0065643f8d16d667c4d0c45d000415842c997
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-10 23:42:48 -0400

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -15,7 +15,7 @@ const Menu = (props) => (
     <li><Link onClick={props.onToggleMenu} to="/contact">Contact Us</Link></li>
             </ul>
             <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Call Us (703) 594-7857</a></li>
+                <li><a href="tel:7035947857" className="button special fit">Call Us (703) 594-7857</a></li>
                 <li><a href="https://secure.lawpay.com/pages/blakeimmigration/trust" className="button fit">Pay Online</a></li>
             </ul>
         </div>
