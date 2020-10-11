238fd2d883f425ea750d8624e36ad6fab6750dcb
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-10 22:35:46 -0400

    Create BannerTestimonials.js

---
+++ src/components/BannerTestimonials.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BannerTestimonials = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Testimonials</h1>
+            </header>
+            <div className="content">
+                <p>Hear from people we've helped</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerTestimonials
