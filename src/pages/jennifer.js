5c8ec88e219e73e6ff0785c03a30704531439c8c
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-11 16:26:53 -0400

    Update jennifer.js

--- src/pages/jennifer.js
+++ src/pages/jennifer.js
@@ -16,7 +16,7 @@ const Jennifer = (props) => (
                 <div className="inner">
                     <header className="major">
                         <h1>Jennifer Araujo</h1>
-    <h3>Associate Attorney</h3>
+    <h3>Senior Paralegal</h3>
                     </header>
                     <img src={pic09} alt="" />
                     <p> Jennifer is a senior paralegal at Blake Immigration Law and has worked at the firm since 2017. She was born and raised in northern Virginia and earned her bachelor’s degree in political science from the Pennsylvania State University. During college she studied abroad in Barcelona, Spain. Jennifer currently attends law school part-time at the University of the District of Columbia (UDC) David A. Clarke School of Law. Jennifer also volunteers at the UDC Immigration and Human Rights Legal Clinic processing applications for adjustment of status and conducting legal research. She is passionate about helping immigrants obtain legal status in the United States and likes interacting with people from different cultural backgrounds. In her free time Jennifer enjoys spending time with her family and baking cakes.</p>
