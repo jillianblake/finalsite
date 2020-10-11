1c4c9c12bbcda19e432aba8dd39a60813d25842d
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-10 22:41:04 -0400

    Create BannerContact.js

---
+++ src/components/BannerContact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BannerServices = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Contact Us</h1>
+            </header>
+            <div className="content">
+                <p>Contact us for a consultation</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerContact
