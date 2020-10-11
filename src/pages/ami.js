239c866c3a1be6362495ca2c89789452c9a5a48c
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-11 16:23:14 -0400

    Update ami.js

--- src/pages/ami.js
+++ src/pages/ami.js
@@ -16,10 +16,10 @@ const Ami = (props) => (
                 <div className="inner">
                     <header className="major">
                         <h1>Ami Patel, Esq.</h1>
-    <h3>Associate Attroney</h3>
+    <h3>Associate Attorney</h3>
                     </header>
                     <img src={pic09} alt="" />
-                    <p>Ami Patel is an associate attorney at Blake Immigration Law. She has worked at the firm since 2018, starting as a law graduate clerk and becoming an associate attorney in 2019. Ami grew up in Louisville, Kentucky and earned her bachelor’s degree in international relations and affairs from American University. She then obtained her law degree from George Washington University Law School. During law school she participated in the George Washington University Law School Immigration Clinic. She also interned at the Open Society Institute in Cambodia, the American Immigration Council, and the American Bar Association. As the daughter of immigrants, Ami loves that her work helps others find safety in the United States. Ami is a member of the California State Bar and the American Immigration Lawyers Association (AILA). In her free time Ami likes to read, hike, and seek out new restaurants.</p>
+                    <p>Ami is an associate attorney at Blake Immigration Law. She has worked at the firm since 2018, starting as a law graduate clerk and becoming an associate attorney in 2019. Ami grew up in Louisville, Kentucky and earned her bachelor’s degree in international relations and affairs from American University. She then obtained her law degree from George Washington University Law School. During law school she participated in the George Washington University Law School Immigration Clinic. She also interned at the Open Society Institute in Cambodia, the American Immigration Council, and the American Bar Association. As the daughter of immigrants, Ami loves that her work helps others find safety in the United States. Ami is a member of the California State Bar and the American Immigration Lawyers Association (AILA). In her free time Ami likes to read, hike, and seek out new restaurants.</p>
                     
                 </div>
             </section>
