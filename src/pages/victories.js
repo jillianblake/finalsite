57f513d9e250fcf39bfb97bbfda0a00480a3d34c
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-11 00:31:25 -0400

    Update victories.js

--- src/pages/victories.js
+++ src/pages/victories.js
@@ -19,11 +19,11 @@ const Victories = (props) => (
     <header className="major">
                       
                     
-    <h2>We have represented clients from all over the world including El Salvador, Honduras, Guatemala, Mexico, Costa Rica, Nicaragua, Venezuela, Colombia, Cuba, Jamaica, Haiti, Peru, Philippines, Pakistan, Azerbaijan, Turkey, Ukraine, Turkmenistan, Iran, Ethiopia, Cameroon, Sierra Leone, Mongolia, Vietnam, South Korea, China and Ghana. Below is a sample of some of our victories: </h2>
+    <h4><p>We have represented clients from all over the world including El Salvador, Honduras, Guatemala, Mexico, Costa Rica, Nicaragua, Venezuela, Colombia, Cuba, Jamaica, Haiti, Peru, Philippines, Pakistan, Azerbaijan, Turkey, Ukraine, Turkmenistan, Iran, Ethiopia, Cameroon, Sierra Leone, Mongolia, Vietnam, South Korea, China and Ghana. Below is a sample of some of our victories: </p></h4>
             </header>
          <h3>2020</h3>
                             <ul>
-                                            <li>Violence Against Women Act (VAWA) petition approved by U.S. Citizenship and Immigration Services (USCIS) for Honduran client who was abused by her U.S. lawful permanent resident husband (2020).
+                                            <li><b>Violence Against Women Act (VAWA) petition</b> approved by U.S. Citizenship and Immigration Services (USCIS) for Honduran client who was abused by her U.S. lawful permanent resident husband (2020).
 </li>
 
 
@@ -31,35 +31,35 @@ const Victories = (props) => (
                    
        <h3>2019</h3>
                             <ul>
-                                            <li>Obtained custody and Special Immigrant Juvenile Status (SIJS) order in Charles County, Maryland Circuit Court for minor El Salvadoran client.</li>
-                                            <li>Cancellation of Removal for Non-Legal Permanent Residents granted for detained Honduran client in Arlington Immigration Court based on the hardship his removal would cause his three U.S. citizen daughters.</li>
-                                            <li>Asylum granted for Honduran client in Arlington Immigration Court based on domestic violence she suffered from her former partner including a violent stabbing and attempted murder.</li>
-                                            <li>Affirmative application for asylum approved at the Arlington Asylum Office for Turkish client based on her fear of political persecution.</li>
+                                            <li><b>Obtained custody and Special Immigrant Juvenile Status (SIJS) order</b> in Charles County, Maryland Circuit Court for minor El Salvadoran client.</li>
+                                            <li><b>Cancellation of Removal for Non-Legal Permanent Residents granted</b> for detained Honduran client in Arlington Immigration Court based on the hardship his removal would cause his three U.S. citizen daughters.</li>
+                                            <li><b>Asylum granted</b> for Honduran client in Arlington Immigration Court based on domestic violence she suffered from her former partner including a violent stabbing and attempted murder.</li>
+                                            <li><b>Affirmative application for asylum approved</b> at the Arlington Asylum Office for Turkish client based on her fear of political persecution.</li>
                                            </ul>
                                            <h3>2018</h3>
                             <ul>
-                                            <li>Cancellation of Removal for Non-Legal Permanent Residents granted in Arlington Immigration Court for Honduran client based on the "exceptional and extremely unusual" hardship his U.S. citizen children, including a son who suffers from muscular dystrophy, would face if he were removed from the United States.</li>
-                                            <li>Application for adjustment of status (green card) approved by U.S. Citizenship and Immigration Services (USCIS) for South Korean client based on her marriage to a U.S. citizen.</li>
-                                            <li>Humanitarian asylum granted for Guatemalan client in Baltimore Immigration Court based on the severity of past persecution she suffered in her home country including child abuse and rape.</li>
+                                            <li><b>Cancellation of Removal for Non-Legal Permanent Residents granted</b> in Arlington Immigration Court for Honduran client based on the "exceptional and extremely unusual" hardship his U.S. citizen children, including a son who suffers from muscular dystrophy, would face if he were removed from the United States.</li>
+                                            <li><b>Application for adjustment of status (green card) approved</b> by U.S. Citizenship and Immigration Services (USCIS) for South Korean client based on her marriage to a U.S. citizen.</li>
+                                            <li><b>Humanitarian asylum granted</b> for Guatemalan client in Baltimore Immigration Court based on the severity of past persecution she suffered in her home country including child abuse and rape.</li>
                  
                                            </ul>
 <h3>2017</h3>
                      <ul>
-                                            <li>Secured lowest possible bond for detained client from Immigration Judge in Arlington Immigration Court.</li>
-                                            <li>Obtained custody and Special Immigrant Juvenile Status (SIJS) orders for two minor El Salvadoran clients in Prince George's County, Maryland Circuit Court.</li>
+                                            <li><b>Secured lowest possible bond</b> for detained client from Immigration Judge in Arlington Immigration Court.</li>
+                                            <li><b>Obtained custody and Special Immigrant Juvenile Status (SIJS) orders</b> for two minor El Salvadoran clients in Prince George's County, Maryland Circuit Court.</li>
                                   
                                            </ul>   
 <h3>2016</h3>
 <ul>
-                                            <li>Asylum granted in Arlington Immigration Court for El Salvadoran mother and child clients based on their fear of domestic violence.</li>
-                                            <li>Registered a foreign custody order and obtained Special Immigrant Juvenile Status (SIJS) orders for three minor Guatemalan clients in Montgomery County, Maryland Circuit Court.</li>
-                                            <li>Motion to Reopen a removal order granted by Baltimore Immigration Court for Honduran client who was not given notice of her removal hearing.</li>
+                                            <li><b>Asylum granted</b> in Arlington Immigration Court for El Salvadoran mother and child clients based on their fear of domestic violence.</li>
+                                            <li><b>Registered a foreign custody order and obtained Special Immigrant Juvenile Status (SIJS) orders</b> for three minor Guatemalan clients in Montgomery County, Maryland Circuit Court.</li>
+                                            <li><b>Motion to Reopen a removal order granted</b> by Baltimore Immigration Court for Honduran client who was not given notice of her removal hearing.</li>
                  
                                     </ul>
  <h3>2015</h3>   
  <ul>
-                                            <li>Legal Permanent Resident (LPR) Cancellation of Removal granted for detained Nicaraguan client in Arlington Immigration Court.</li>
-                                            <li>Violence Against Women Act (VAWA) petition approved by U.S. Citizenship and Immigration Services (USCIS) for Filipina client who was abused by her U.S. citizen husband.</li>
+                                            <li><b>Legal Permanent Resident (LPR) Cancellation of Removal granted</b> for detained Nicaraguan client in Arlington Immigration Court.</li>
+                                            <li><b>Violence Against Women Act (VAWA) petition approved</b> by U.S. Citizenship and Immigration Services (USCIS) for Filipina client who was abused by her U.S. citizen husband.</li>
                                   
                                            </ul>   
    </div>
