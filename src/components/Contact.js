a7d0bcd16cdc9ff02240ac6e6af031c88cca5506
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-12 12:08:08 -0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,7 +24,7 @@ const Contact = (props) => (
                         <li><input type="reset" value="Clear" /></li>
                     </ul>
                 </form>
-    <img src={pic16} width="100" height="100" alt="" />
+    <img src={pic16} width="200" height="200" alt="" />
 
             </section>
             <section className="split">
