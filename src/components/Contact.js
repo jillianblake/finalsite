1bb09afe550bdbbad61912a63cd8f09e72384ea9
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-14 23:00:53 -0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                <form method="post" action="#">
+                <form method="post" action="mailto:jillian@blakeimmigration.com">
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
                         <input type="text" name="name" id="name" />
