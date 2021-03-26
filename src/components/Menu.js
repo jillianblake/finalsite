2e3a7128acab1c1c4f744ad2229e53fa59b82542
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2021-03-26 17:21:54 -0400

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,12 +7,12 @@ const Menu = (props) => (
         <div className="inner">
             <ul className="links">
                 <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/about">About Us</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/services">Services</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/testimonials">Testimonials</Link></li>
-    <li><Link onClick={props.onToggleMenu} to="/victories">Victories</Link></li>
-    <li><Link onClick={props.onToggleMenu} to="/social">Social Media</Link></li>
-    <li><Link onClick={props.onToggleMenu} to="/contact">Contact Us</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/about">About Us <i>Acerca de</i></Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/services">Services<i>Servicios</i></Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/testimonials">Testimonials<i>Testimonios</i></Link></li>
+    <li><Link onClick={props.onToggleMenu} to="/victories">Victories<i>Victorias</i></Link></li>
+    <li><Link onClick={props.onToggleMenu} to="/social">Social Media<i>Redes Sociales</i></Link></li>
+    <li><Link onClick={props.onToggleMenu} to="/contact">Contact Us<i>Contacto</i></Link></li>
             </ul>
             <ul className="actions vertical">
                 <li><a href="tel:7035947857" className="button special fit">Call Us (703) 594-7857</a></li>
