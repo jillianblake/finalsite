d259aa7ad1df1e5b040084dd03d85ee224b12470
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2021-04-26 21:29:10 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -51,12 +51,12 @@ const About = (props) => (
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Ami Patel, Esq.</h3>
+                                <h3>Amelia Wester, Esq.</h3>
                                 <h4>Associate Attorney</h4>
                             </header>
-                            <p>Ami is an associate attorney at Blake Immigration Law. She has worked at the firm since 2018, starting as a law graduate clerk and becoming an associate attorney in 2019. Ami grew up in Louisville, Kentucky and earned her bachelor’s degree in international relations and affairs from American University. She then obtained her law degree from George Washington University Law School. During law school she participated in the George Washington University Law School Immigration Clinic. She also interned at the Open Society Institute in Cambodia, the American Immigration Council, and the American Bar Association. As the daughter of immigrants, Ami loves that her work helps others find safety in the United States. Ami is a member of the California State Bar and the American Immigration Lawyers Association (AILA). In her free time Ami likes to read, hike, and seek out new restaurants.</p>
+                            <p> </p>
                             <ul className="actions">
-                                <li><a href="mailto:ami@blakeimmigration.com" className="button fit">ami@blakeimmigration.com</a></li>
+                                <li><a href="mailto:amelia@blakeimmigration.com" className="button fit">amelia@blakeimmigration.com</a></li>
                             </ul>
                         </div>
                     </div>
