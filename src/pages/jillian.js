d11aab604bf0935c638ee4736e56b7fd35f85c85
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2021-04-26 21:08:05 -0400

    Update jillian.js

--- src/pages/jillian.js
+++ src/pages/jillian.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
-import pic08 from '../assets/images/pic08.jpg'
+import pic18 from '../assets/images/pic18.jpg'
 
 const Jillian = (props) => (
     <Layout>
@@ -18,7 +18,7 @@ const Jillian = (props) => (
                         <h1>Jillian Blake, Esq.</h1>
     <h3>Attorney/Owner</h3>
                     </header>
-                    <img src={pic08} alt="" />
+                    <img src={pic18} alt="" />
                     <p>Jillian is an attorney and owner of Blake Immigration Law since 2015. She grew up in San Francisco, California and earned her bachelor’s degree in international studies from Johns Hopkins University and master’s degree in international relations from the Johns Hopkins School of Advanced International Studies (SAIS). She then obtained her law degree from the University of Michigan Law School. During law school she discovered her passion for immigration and asylum law while interning at Asylum Access—Ecuador and Capital Area Immigrants’ Rights (CAIR) Coalition. Jillian is a member of the Maryland State Bar and the American Immigration Lawyers Association (AILA). She is also currently an adjunct professor of immigration law at George Mason University Law School in Arlington, Virginia. In her free time Jillian enjoys watching movies, writing, watching Cleveland Browns football and Nationals baseball, and spending time with her dog, Cha Cha.</p>
                     
                 </div>
