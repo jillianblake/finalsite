de48fdaf708948defb35dca8292654dd9c69dd83
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2021-05-02 21:53:22 -0400

    Update testimonials.js

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -23,15 +23,16 @@ const Testimonials = (props) => (
                         <div className="col-6">
                             
                             <p>“The lawyer I had in Miami when I got out of the detention center recommended [Blake] Immigration Law. My experience with the firm has always been very good even from the consult when Jillian explained to me the process of my case. Jillian is a great lawyer, the fact that she speaks to me in Spanish is huge and helped me feel comfortable. In my case, it was favorable, and I owe that Jillian’s dedication and the fact that’s she’s always been nothing but a professional. My outcome was favorable I was granted asylum. The best thing about my case is that I am now legally here in the United States and also having my family able to be legally in the U.S."</p>
-                           <p> -N.B.</p>
+                           -N.B.
                         </div>
+    
       <div className="grid-wrapper">
                
                         <div className="col-6">
                             
                             <i><p>"La abogada que tuve en Miami cuando salí del centro de detención me recomendó [Blake] Immigration Law. Mi experiencia con el bufete siempre ha sido muy buena incluso desde la consulta cuando Jillian me explicó el proceso de mi caso. Jillian es una gran abogada, el hecho de que ella me hable en español es enorme y me ayudó a sentirme cómoda. En mi caso, fue favorable, y le debo la dedicación de Jillian y el hecho de que siempre ha sido más que una profesional. Mi resultado fue favorable, me concedieron asilo. Lo mejor de mi caso es que ahora estoy legalmente aquí en los Estados Unidos y que mi familia también puede estar legalmente en los Estados Unidos." -N.B.
 </p></i>
-                            <p>-N.B.</p>
+                            -N.B.
                         </div>
                         <div className="col-6">
                             
