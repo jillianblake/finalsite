79f0d82cc3c72109f7ce2b9f4573328c78829b91
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2021-05-02 20:44:02 -0400

    Update BannerServices.js

--- src/components/BannerServices.js
+++ src/components/BannerServices.js
@@ -5,9 +5,10 @@ const BannerServices = (props) => (
         <div className="inner">
             <header className="major">
                 <h1>Services</h1>
+    <h1>Servicios</h1>
             </header>
             <div className="content">
-                <p>Removal Defense, Humanitarian Visas, Family-Based Petitions, Naturalization</p>
+                
             </div>
         </div>
     </section>
