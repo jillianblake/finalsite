8fc17da331a34b49c1075548096d04c075341f21
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2021-05-02 20:47:45 -0400

    Update BannerSocial.js

--- src/components/BannerSocial.js
+++ src/components/BannerSocial.js
@@ -5,9 +5,10 @@ const BannerSocial = (props) => (
         <div className="inner">
             <header className="major">
                 <h1>Social Media</h1>
+    <i><h1>Redes Sociales</h1></i>
             </header>
             <div className="content">
-                <p>Stay up to date</p>
+           
             </div>
         </div>
     </section>
