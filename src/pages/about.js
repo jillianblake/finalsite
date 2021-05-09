1baa74db20fea8005d36a9424322f6b6d3bf6af9
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2021-05-09 19:30:44 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -133,7 +133,8 @@ const About = (props) => (
                                 <h3>Diana Maravilla</h3>
                                 <h4>Receptionist/Administrative Assistant</h4>
                             </header>
-                            <p> </p>
+                            <p> Diana has been a receiptionist and administrative assistant at Blake Immigration Law since 2021. Diana grew up in Arlington, Virginia and earned her Immigration Law Specialist Paralegal Certificate at the Washington Online Learning Institute. Read more about Diana... </p>
+    <i><p>Diana ha sido recepcionista y asistente administrativa en Blake Immigration Law desde 2021. Diana creció en Arlington, Virginia y obtuvo su Certificado de asistente legal de especialista en derecho de inmigración en el Washington Online Learning Institute. Leer más sobre Diana... </i></p>
                             <ul className="actions">
                                 <li><a href="mailto:diana@blakeimmigration.com" className="button fit">diana@blakeimmigration.com</a></li>
                             </ul>
