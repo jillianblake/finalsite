5b3adef8b821d3184613aa9c0829a65ae3f192e0
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2021-06-30 22:26:21 -0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,24 +6,7 @@ const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                <form method="post" action="#" data-netlify="true">
-                    <div className="field half first">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" />
-                    </div>
-                    <div className="field half">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" />
-                    </div>
-                    <div className="field">
-                        <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows="6"></textarea>
-                    </div>
-                    <ul className="actions">
-                        <li><input type="submit" value="Send Message" className="special" /></li>
-                        <li><input type="reset" value="Clear" /></li>
-                    </ul>
-                </form>
+                
     <img src={pic16} width="200" height="200" alt="" />
 
             </section>
