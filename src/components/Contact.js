51b0b5a76159957837f6247811de45fa0d95c5ce
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2021-12-16 23:27:58 -0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -30,11 +30,11 @@ const Contact = (props) => (
                         <span className="icon alt fa-home"></span>
                         <h3>Addresses</h3>
                         <span><b>Mailing Address</b><br />
-                            1727 King St. Ste 300<br />
+                            107 S. West St. #810<br />
                         Alexandria, VA 22314<br />
                         </span>
 <span><b>Physical Address-Alexandria</b><br />
-                            107 S. West St. #108<br />
+                            1517 King St.<br />
                         Alexandria, VA 22314<br />
                         </span>
 <span><b>Physical Address-Baltimore</b><br />
