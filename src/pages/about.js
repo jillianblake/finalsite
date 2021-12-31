6d9917b0b966c1bfe0e05573c1466d1583c5f3cf
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2021-12-31 14:14:12 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ import pic20 from '../assets/images/pic20.jpg'
 import pic11 from '../assets/images/pic11.jpg'
 import pic21 from '../assets/images/pic21.jpg'
 import pic22 from '../assets/images/pic22.jpg'
-import pic25 from '../assets/images/pic25.jpg'
+import pic24 from '../assets/images/pic24.jpg'
 
 
 const About = (props) => (
@@ -67,7 +67,7 @@ const About = (props) => (
                 </section>
                 <section>
                     <Link to="/shane" className="image">
-                        <img src={pic25} alt="" />
+                        <img src={pic24} alt="" />
                     </Link>
                     <div className="content">
                         <div className="inner">
