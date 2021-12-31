845375774105f6646611022929409c0e165cc091
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2021-12-31 11:13:08 -0500

    Update shane.js

--- src/pages/shane.js
+++ src/pages/shane.js
@@ -1,1 +1,31 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
 
+import pic24 from '../assets/images/pic24.jpg'
+
+const Shane = (props) => (
+    <Layout>
+        <Helmet>
+            <title>Shane</title>
+            <meta name="description" content="Shane Page" />
+        </Helmet>
+
+        <div id="main" className="alt">
+            <section id="one">
+                <div className="inner">
+                    <header className="major">
+                        <h1>Shane Ivey, Esq.</h1>
+    <h3>Associate Attorney</h3>
+                    </header>
+                    <img src={pic24} alt="" />
+                    <p>English Bio</p>
+                    <i><p>Spanish Bio</p></i>
+                </div>
+            </section>
+        </div>
+
+    </Layout>
+)
+
+export default Shane
