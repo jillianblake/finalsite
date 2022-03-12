5276d9b39a1d41a0cc94d957eaf798d2d6c19693
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2022-03-12 16:15:56 -0500

    Create blog.js

---
+++ src/pages/blog.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+import BannerBlog from '../components/BannerBlog'
+
+const Blog = (props) => (
+    <Layout>
+        <Helmet>
+            <title>Blog</title>
+            <meta name="description" content="Blog" />
+        </Helmet>
+    
+<BannerBlog />
+   
+ <div id="main" className="alt">
+
+            <section id="one">
+                <div className="inner">
+   
+                 Blog coming soon...
+</section>
+</div>
+                   
+   
+                
+
+        
+    </Layout>
+)
+
+export default Blog
