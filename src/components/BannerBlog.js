e682c7800c34e292ee50baca0246fbf5eca81ab5
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2022-03-12 16:13:22 -0500

    Create BannerBlog.js

---
+++ src/components/BannerBlog.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BannerBlog = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Blog</h1>
+   <i><h1>El Blog</h1></i>
+            </header>
+            <div className="content">
+                
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerBlog
