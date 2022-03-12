9728c26f905c5788d99441f0181a03bdd6fba70d
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2022-03-12 13:13:15 -0500

    Create BannerGallery.js

---
+++ src/components/BannerGallery.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const BannerGallery = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Victory Gallery</h1>
+                    <i><h1>Galería de las Victorias</h1></i>
+
+            </header>
+            <div className="content">
+               
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerGallery
